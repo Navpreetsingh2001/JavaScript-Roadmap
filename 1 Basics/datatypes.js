753d48c37f24eec459a9b93901ce160f8a954286
Navpreet singh <122290367+Navpreetsingh2001@users.noreply.github.com>
2024-02-21 10:47:20 +0530

    Create datatypes.js

---
+++ 1 Basics/datatypes.js
@@ -0,0 +1,33 @@
+// ! Datatypes 
+//* js variable can hold numbers like 100 and text values like "Navy" 
+// * If you put a number in quotes, it will be treated as a text string 
+const pi =3.14;
+let name = "navy";
+
+// ! one statement many variables
+let name1 = "navy" , car = "volvo" ,price =$3000;
+// or 
+let name2 = "navy" ,
+ car1 = "volvo" ,
+ price1 =$3000;
+
+
+//  ! undefined
+//* In computer programs, variables are often declared without a value. 
+// *The value can be something that has to be calculated, or something that will be provided later, like user input.
+
+//* A variable declared without a value will have the value undefined.
+let stranger;
+//  * it is storing the undefined 
+
+// ! re defining js variable 
+var carName = "Volvo";
+var carName;
+//  * If you re-declare a JavaScript variable declared with var, it will not lose its value.
+// * You cannot re-declare a variable declared with let or const 
+
+// ! JavaScript Dollar Sign $ 
+// * Since JavaScript treats a dollar sign as a letter, identifiers containing $ are valid variable names 
+let $ = "Hello World";
+let $$$ = 2;
+let $myMoney = 5;
