9332c5ae74048a38b429926ef42b7b10bca5b766
Navpreet singh <122290367+Navpreetsingh2001@users.noreply.github.com>
2024-03-01 01:52:38 +0530

    Create 02datatypes.js

---
+++ 01_Basics/02datatypes.js
@@ -0,0 +1,20 @@
+"use strict ";  // treat all js code as newer version
+
+// alert("hello") // we are using nodejs not browser 
+
+let name = " Rahul "
+let age = 18
+let isLoggedIn =false
+
+// number => 2 to power 53]
+//bigint => whee number is big
+//string =>" "
+//boolean => true/false
+//null => standalone value
+//undefined => no value assign 
+//symbol => uniqueness
+
+
+//object 
+console.log(typeof null);  // type object
+console.log(typeof undefined);  // type undefined
