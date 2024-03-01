6fe23d2415952587ca3712f165f63cecdba84c90
Navpreet singh <122290367+Navpreetsingh2001@users.noreply.github.com>
2024-03-02 01:57:13 +0530

    Update 03_consversionDatatype.js

--- 01_Basics/03_consversionDatatype.js
+++ 01_Basics/03_consversionDatatype.js
@@ -26,3 +26,42 @@ let someNumber = 33;
 let convertToString = String(someNumber);
 console.log(convertToString);
 console.log(typeof convertToString);
+
+// **************opertion*******************
+
+let value = 3
+let negVal = -value
+console.log(negVal)
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**2)
+// console.log(2/3)
+// console.log(2%3)
+
+let str1 = "Navy"
+let str2 = " Rajput"
+let str3 = str1+str2
+console.log(str3);
+
+console.log("1"+ 2);
+console.log(1 + "2")
+console.log("1" + 2 + 2) ;
+console.log(1 + 2 + "2") ;
+
+// console.log(+true);
+// console.log(+ "");
+
+let num1,num2,num3
+
+num1=num2=num3= 2+2
+// console.log(num3); 
+
+let gameCounter =100;
+// postfix 
+gameCounter++;
+// prefix 
+++gameCounter;
+
+console.log(gameCounter);
