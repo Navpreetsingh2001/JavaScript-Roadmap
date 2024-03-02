b936f600604b9ad198e74045096dc7ddd55c5871
Navpreet singh <122290367+Navpreetsingh2001@users.noreply.github.com>
2024-03-02 23:58:32 +0530

    Add files via upload

---
+++ 01_Basics/04_comparison.js
@@ -0,0 +1,17 @@
+console.log("2" > 1);
+console.log("02" > 1);
+
+console.log(null > 0 ); // here null is converted to a number it as 0
+console.log(null == 0);
+console.log(null >= 0); // thatswhy here the result is true.
+
+
+
+console.log(undefined == 0 )
+console.log(undefined >= 0 )
+console.log(undefined < 0 )
+
+// === 
+
+console.log("2" == 2);
+console.log("2" === 2)
