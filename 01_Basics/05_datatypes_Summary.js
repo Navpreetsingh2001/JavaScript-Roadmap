f9442e932a9f80cb7e7e82a30a3bd72a043788ec
Navpreet singh <122290367+Navpreetsingh2001@users.noreply.github.com>
2024-03-03 00:40:40 +0530

    Add files via upload

---
+++ 01_Basics/05_datatypes_Summary.js
@@ -0,0 +1,56 @@
+// types 
+// Primitive    non Primitive or refrence type
+
+
+    // 7 Types:
+    // They all are call by value .They are not original but the copy is given and changes are made in copy.
+
+    // String 
+    // Number
+    // Boolean
+    // Null -->empty not 0
+    // Undefined
+    // Symbol
+    // BigInt
+
+    const score =100
+    const scoreVal =100.01
+
+    const logedIn = false
+    const temp = null
+    let userEmail; 
+    // or 
+    let userEmail1 =undefined
+
+    const id = Symbol('123')
+    const anotherId = Symbol('123')
+    console.log(id === anotherId) // They are not same
+
+    const bigNumber = 1213333333333333333n
+
+    //Array
+    const heros = ["hello" , "world"]
+
+    //objects
+    let myObj={
+       name : "navy",
+       age : 22, 
+    }
+
+    //functions
+    const myFunction = function(){
+        console.log("hello")
+    }
+    console.log(typeof bigNumber);
+    console.log(typeof temp);
+
+
+    // 2. Refrence or non primitive 
+
+    // Array
+    // Objects
+    // Functions
+
+    // Q-> Is js dynamically type or statically type language
+    // A language is dynamically typed if the type is associated with run-time values, and not named variables/fields/etc.
+
