10943ac28ac1807741b03d712314aad4b060a634
Navpreet singh <122290367+Navpreetsingh2001@users.noreply.github.com>
2024-03-20 15:22:22 +0530

    Add files via upload

---
+++ 03 basics/05_codeExecute_in_js.js
@@ -0,0 +1,37 @@
+//! Js execution context 
+
+
+// javascript is single threaded 
+
+//! javascript runs in two phases 
+
+// 1. phase (Memory creation phase) 
+// 2. Execution phase 
+
+/* 
+*code --->  
+*global execution context (this variable)
+ *--> memory phase -->
+ val1 -> undefined
+ val2 ->undefined
+ addnum ->defination
+ result1 ->undefined
+ result2 ->undefined
+
+ * execution phase
+ val1 = 10
+ val2 = 5
+ result1
+
+
+
+
+ */
+
+
+//* -->global EC 
+//* -->Function EC
+//* -->Eval EC 
+
+
+// in browser this value is given as window 
