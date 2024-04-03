e1548e87cace351256d8c573271a01b1f7c35f57
Navpreet singh <122290367+Navpreetsingh2001@users.noreply.github.com>
2024-04-03 10:19:20 +0530

    Update 02_Promises.js
    updated

--- 09 Advance 1/02_Promises.js
+++ 09 Advance 1/02_Promises.js
@@ -66,7 +66,7 @@ promiseThree.then(function(data){
 
 // * promise 5
 
-const promiseFour = new Promise(function(resolve ,reject){
+const promiseFive = new Promise(function(resolve ,reject){
     setTimeout(function(){
         const error = true
         if(!error){
@@ -77,3 +77,37 @@ const promiseFour = new Promise(function(resolve ,reject){
         }
     },1000)
 })
+
+async function consumeFunctionFive(){
+    try{
+        const response = await promiseFive
+        console.log(response);
+    }
+    catch(error){
+        console.log(error);
+    }
+}
+consumeFunctionFive();
+
+// async function allData(){
+//     try {
+//         const response = await fetch('https://jsonplaceholder.typicode.com/users')
+//         const data =await response.json()
+//         console.log(data);
+        
+//     } catch (error) {
+//         console.log('E:' ,error);
+//     }
+// }
+// allData();
+
+// * using try catch 
+
+fetch('https://api.github.com/users/navpreetsingh2001')
+.then((response)=>{
+    return response.json();
+})
+.then((data) => {
+    console.log(data);
+})
+.catch((error) => console.log(error))
