53ec4cd5dad94a157a51d7ca091108942c61a44e
Navpreet singh <122290367+Navpreetsingh2001@users.noreply.github.com>
2024-04-23 23:45:27 +0530

    Add files via upload

---
+++ 100xdev js/level2/02-dates.js
@@ -0,0 +1,10 @@
+ 
+ const currentDate = new Date();
+ console.log(currentDate);
+ console.log(currentDate.getDate());
+ console.log(currentDate.getFullYear());
+
+
+ console.log("Hours : "+currentDate.getHours());
+ console.log("Hours : "+currentDate.getMinutes());
+ console.log("Hours : "+currentDate.getSeconds());
